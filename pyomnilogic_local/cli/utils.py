4f63d53bc0b36dab039e0e1e648b01dc370ba76f
Chris Jowett <421501+cryptk@users.noreply.github.com>
2025-10-21 20:37:31 -0500

    fix: remove functools cache from CLI

--- pyomnilogic_local/cli/utils.py
+++ pyomnilogic_local/cli/utils.py
@@ -1,18 +1,14 @@
-from functools import cache
-
 from pyomnilogic_local.api import OmniLogicAPI
 from pyomnilogic_local.models.mspconfig import MSPConfig
 from pyomnilogic_local.models.telemetry import Telemetry
 
 
-@cache
 async def async_get_mspconfig(omni: OmniLogicAPI, raw: bool = False) -> MSPConfig:
     mspconfig: MSPConfig
     mspconfig = await omni.async_get_config(raw=raw)
     return mspconfig
 
 
-@cache
 async def async_get_telemetry(omni: OmniLogicAPI, raw: bool = False) -> Telemetry:
     telemetry: Telemetry
     telemetry = await omni.async_get_telemetry(raw=raw)
