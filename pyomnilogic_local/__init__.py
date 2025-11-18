a71cdb9f92313b479e554740373128547c79c826
Chris Jowett <421501+cryptk@users.noreply.github.com>
2025-11-18 00:11:03 -0600

    fix: add some convenience imports to init.py

--- pyomnilogic_local/__init__.py
+++ pyomnilogic_local/__init__.py
@@ -2,8 +2,25 @@
 
 from __future__ import annotations
 
+from ._base import OmniEquipment
+from .backyard import Backyard
+from .bow import Bow
+from .chlorinator import Chlorinator
+from .chlorinator_equip import ChlorinatorEquipment
 from .collections import EffectsCollection, LightEffectsCollection
+from .colorlogiclight import ColorLogicLight
+from .csad import CSAD
+from .csad_equip import CSADEquipment
+from .filter import Filter
+from .groups import Group
+from .heater import Heater
+from .heater_equip import HeaterEquipment
 from .omnilogic import OmniLogic
+from .pump import Pump
+from .relay import Relay
+from .schedule import Schedule
+from .sensor import Sensor
+from .system import System
 from .util import (
     OmniConnectionError,
     OmniEquipmentNotInitializedError,
@@ -12,11 +29,28 @@ from .util import (
 )
 
 __all__ = [
+    "CSAD",
+    "Backyard",
+    "Bow",
+    "CSADEquipment",
+    "Chlorinator",
+    "ChlorinatorEquipment",
+    "ColorLogicLight",
     "EffectsCollection",
+    "Filter",
+    "Group",
+    "Heater",
+    "HeaterEquipment",
     "LightEffectsCollection",
     "OmniConnectionError",
+    "OmniEquipment",
     "OmniEquipmentNotInitializedError",
     "OmniEquipmentNotReadyError",
     "OmniLogic",
     "OmniLogicLocalError",
+    "Pump",
+    "Relay",
+    "Schedule",
+    "Sensor",
+    "System",
 ]
